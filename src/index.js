4c1af1bbe36f40ed9529486b22cb011a2c9a8083
JuliaKhvisevich <reytjulia@gmail.com>
2024-08-06 22:02:38 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  const positiveNumber = Math.abs(n);
+  const stringNumber = String(positiveNumber);
+  const splitNumber = stringNumber.split('');
+  const reverseNumber = splitNumber.reverse();
+  if (reverseNumber[0] === '0') {
+    delete reverseNumber[0];
+  };
+  const newTextNumber = reverseNumber.join('');
+  const newNumber = Number(newTextNumber);
+  return newNumber;
 }
